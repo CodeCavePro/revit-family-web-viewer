93b65cb27847accf7d1f0c41aa2b664d86241fae
salaros <salaros@yandex.ru>
2017-11-03 21:51:19 +0300

    an example of how to override default web viewer options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { WebViewer } from './WebViewer';
+import { WebViewerOptions } from './WebViewerOptions';
 
 let rwv = new WebViewer();
+rwv.options = new WebViewerOptions(
+    'linear-gradient(141deg, #5D8CAE 0%, #317589 51%, #4D8FAC 75%)',
+    true,
+    0.015
+);
 rwv.init( document.getElementById('main') );
 rwv.loadModelFromUrl( './dist/assets/models/6-Burner_Gas_Stove.json' );
 console.log('Revit Web Viewer has been initialized successfully!');
