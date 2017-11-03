4964fd60ea92000cef3004367697e4ca8c6316f2
salaros <salaros@yandex.ru>
2017-11-03 20:09:08 +0300

    implements SCSS compilation and CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var bro = require('gulp-bro');
+var scss = require('gulp-scss');
+var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
@@ -15,7 +17,7 @@ gulp.task('ts-scripts', function() {
         .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('browserify', ['ts-scripts'], function() {
+gulp.task('browserify', [ 'ts-scripts' ], function() {
     gulp.src('./dist/js/index.js')
     .pipe(sourcemaps.init({largeFile: true, loadMaps: true}))
     .pipe(bro())
@@ -24,10 +26,36 @@ gulp.task('browserify', ['ts-scripts'], function() {
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('watch', ['browserify'], function() {
-    gulp.watch('./src/*.ts', ['browserify']);
+gulp.task('scss', function () {
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init({largeFile: true, loadMaps: false}))
+        .pipe(scss({
+            // "bundleExec": true
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['browserify'], function() {
+gulp.task('css-minify', [ 'scss' ], function () {
+
+    gulp.src([
+            './dist/css/**/*.css',
+            '!./dist/css/**/*.min.css'
+        ])
+        .pipe(sourcemaps.init({largeFile: true, loadMaps: false}))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: ".min",
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', [ 'browserify', 'css-minify' ], function() {
+    gulp.watch('./src/*.ts', [ 'browserify' ]);
+    gulp.watch('./src/*.scss', [ 'css-minify' ]);
+});
+
+gulp.task('default', [ 'browserify', 'css-minify' ], function() {
     // Just build ts-scripts and run browserify
 });
