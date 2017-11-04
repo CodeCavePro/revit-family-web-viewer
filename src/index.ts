0ec37b498dc76f61555673128160f99dec6e8e20
salaros <salaros@yandex.ru>
2017-11-05 01:27:35 +0300

    slowing down the rotation of the model

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ let rwv = new WebViewer();
 rwv.options = new WebViewerOptions(
     'linear-gradient(141deg, #5D8CAE 0%, #317589 51%, #4D8FAC 75%)',
     true,
-    0.015
+    0.0035
 );
 rwv.init( document.getElementById('main') );
 rwv.loadModelFromUrl( './dist/models/6-Burner_Gas_Stove.json' );
